cd0aef6dd8fcad9e3f92d8ee5ca51c574ae184a5
julz <julz@roxies-MacBook-Pro.local>
2019-05-04 08:25:32 +1000

    form field added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import React from 'react';
 import './App.css';
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          {this.props.name}
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 function Icon(props) {
 	return (
-		<button 
-			className={props.value ? "green" : "red" }
-			onClick={props.onClick}
-		>
-			{props.value}
-		</button>
+    <div>
+      <button 
+        className={props.value ? "green" : "red" }
+        onClick={props.onClick}
+      >
+        {props.name}
+      </button>
+      <NameForm name={props.name}/>
+    </div>
 	);
 }
+
 
 class App extends React.Component {
 
   constructor(props) {
 		super(props);
 		this.state = {
-			toggled: Array(3).fill(0),
+      toggled: Array(3).fill(0),
+      name: ["first name", "second name", "last name"]
 		}
   }
 
   renderIcon(i) {
 	  return <Icon 
 	  value={this.state.toggled[i]}
+    name={this.state.name[i]}
 	  onClick={() => this.handleClick(i)}
 	  />;  // anything you set here can be accessed
 	}
