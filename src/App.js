2d7191d5b7c980142ec46e12dc5a12cd7459cbfe
julz <julz@roxies-MacBook-Pro.local>
2019-05-03 22:02:34 +1000

    basic skeleton for the sidebar is functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square(props) {
+	return (
+		<button 
+			className="square"
+			onClick={props.onClick}
+		>
+			{props.value}
+		</button>
+	);
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+		super(props);
+		this.state = {
+			toggled: Array(3).fill(0),
+		}
+  }
+
+  renderSquare(i) {
+	  return <Square 
+	  value={this.state.toggled[i]}
+	  onClick={() => this.handleClick(i)}
+	  />;  // anything you set here can be accessed
+	}
+
+  handleClick(i) {
+    const newToggled = this.state.toggled.fill(0);
+    newToggled[i] = 1;
+    console.log(i)
+    this.setState({
+      toggled: newToggled,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+      </div>
+    );
+  }
 }
 
 export default App;
